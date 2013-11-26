726d9aef3212f348fb60a640d110ba0cf49621e5
mx3L <mx3ldev@gmail.com>
2013-11-26 11:50:45 +0100

    fixed context menu

--- build/plugin/src/engine/handlers/context.py
+++ build/plugin/src/engine/handlers/context.py
@@ -1,4 +1,5 @@
 from item import ItemHandler
+from folder import FolderItemHandler
 from Plugins.Extensions.archivCZSK.gui.exception import AddonExceptionHandler
 from Plugins.Extensions.archivCZSK.engine.items import PContextMenuItem
 
@@ -7,24 +8,11 @@ class ContextMenuItemHandler(ItemHandler):
     def __init__(self, session, content_screen, content_provider):
         ItemHandler.__init__(self, session, content_screen)
         self.content_provider = content_provider
+        self.folder_handler = FolderItemHandler(session, content_screen, content_provider)
     
-    def open_item(self, item, *args, **kwargs):
-        """executes context menu items"""
+    def _open_item(self, item, *args, **kwargs):
         if item.can_execute():
             return item.execute()
-        
-        def run_item_success_cb(result):
-            list_items, screen_command, args = result
-            self.content_screen.resolveCommand(screen_command, args)
-            
-        @AddonExceptionHandler(self.session)
-        def run_item_error_cb(failure):
-            self.content_screen.stopLoading()
-            self.content_screen.showList()
-            self.content_screen.workingFinished()
-            failure.raiseException()
-                 
-        self.content_screen.workingStarted()
-        self.content_screen.startLoading()
-        self.content_screen.hideList()
-        self.content_provider.get_content(self.session, item.get_params(), run_item_success_cb, run_item_error_cb)
+        else:
+            item.params = item.get_params()
+            return self.folder_handler._open_item(item, *args, **kwargs)
