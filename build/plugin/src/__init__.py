758b3fd3ea095052f23b0fd0e800588bae330901
mx3L <mx3ldev@gmail.com>
2014-06-12 18:27:21 +0200

    don't show "fallback to default translation" messages

--- build/plugin/src/__init__.py
+++ build/plugin/src/__init__.py
@@ -3,23 +3,23 @@ from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
 import os, gettext, sys
 #from logging import StreamHandler
- 
+
 PluginLanguageDomain = "archivCZSK"
 PluginLanguagePath = "Extensions/archivCZSK/locale"
- 
+
 def localeInit():
     lang = language.getLanguage()[:2] # getLanguage returns e.g. "fi_FI" for "language_country"
     os.environ["LANGUAGE"] = lang # Enigma doesn't set this (or LC_ALL, LC_MESSAGES, LANG). gettext needs it!
     print "[WebInterface] set language to ", lang
     gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
- 
+
 def _(txt):
     t = gettext.dgettext(PluginLanguageDomain, txt)
     if t == txt:
-        print "[%s] fallback to default translation for %s" % (PluginLanguageDomain, txt)
+        #print "[%s] fallback to default translation for %s" % (PluginLanguageDomain, txt)
         t = gettext.gettext(txt)
     return t
-    
+
 localeInit()
 language.addCallback(localeInit)
 
@@ -28,11 +28,11 @@ class log(object):
     INFO = 1
     DEBUG = 2
     mode = INFO
-    
+
     @staticmethod
     def changeMode(mode):
         log.mode = mode
-    
+
     @staticmethod
     def debug(text, *args):
         if log.mode == log.DEBUG:
@@ -41,17 +41,17 @@ class log(object):
             elif len(args) >=1:
                 text = text % args
             print "[ArchivCZSK] DEBUG:", text.encode('utf-8')
-    
-    @staticmethod    
+
+    @staticmethod
     def info(text, *args):
         if log.mode >= log.INFO:
             if len(args) == 1 and isinstance(args[0], tuple):
                 text = text % args[0]
             elif len(args) >=1:
                 text = text % args
-            print "[ArchivCZSK] INFO:", text.encode('utf-8') 
-    
-    @staticmethod  
+            print "[ArchivCZSK] INFO:", text.encode('utf-8')
+
+    @staticmethod
     def error(text, *args):
         if log.mode >= log.ERROR:
             if len(args) == 1 and isinstance(args[0], tuple):
@@ -59,7 +59,7 @@ class log(object):
             elif len(args) >=1:
                 text = text % args
             print "[ArchivCZSK] ERROR:", text.encode('utf-8')
-  
+
 
 # set logger
 #default_formatter = logging.Formatter("%(asctime)s:%(levelname)s:%(message)s")
