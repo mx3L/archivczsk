d5472e8383c1d744d20760d25d8533b7fdeca418
mx3L <mx3ldev@gmail.com>
2016-08-20 17:05:25 +0200

    plugin: refactoring
    - only one WHERE_PLUGINMENU function
    - cleanup whitespaces
    - organize imports
    - dont use version module(will be removed)
    - saner variable names

--- build/plugin/src/plugin.py
+++ build/plugin/src/plugin.py
@@ -1,62 +1,49 @@
 # -*- coding: UTF-8 -*-
-'''
-Created on 28.2.2012
-
-@author: marko
-'''
-from Plugins.Plugin import PluginDescriptor
-from Screens.PluginBrowser import *
-from Components.PluginComponent import plugins
 from Components.config import config
+from Plugins.Plugin import PluginDescriptor
 
 from Plugins.Extensions.archivCZSK import _
-import gui.download as dwnld
-from archivczsk import ArchivCZSK
-from engine.downloader import DownloadManager
-from gui.search import ArchivCZSKSearchClientScreen
-import version
-from gsession import GlobalSession
+from Plugins.Extensions.archivCZSK.archivczsk import ArchivCZSK
+from Plugins.Extensions.archivCZSK.gsession import GlobalSession
+from Plugins.Extensions.archivCZSK.gui.search import ArchivCZSKSearchClientScreen
+from Plugins.Extensions.archivCZSK.engine.downloader import DownloadManager
 
-def sessionstart(reason, session):
+NAME = _("ArchivCZSK")
+DESCRIPTION = _("Playing CZ/SK archives")
+
+def sessionStart(reason, session):
 	GlobalSession.setSession(session)
-	#saving active downloads to session
+	# saving active downloads to session
 	if not hasattr(session, 'archivCZSKdownloads'):
-		session.archivCZSKdownloads = []	
+		session.archivCZSKdownloads = []
 	if DownloadManager.getInstance() is None:
 		DownloadManager(session.archivCZSKdownloads)
 
-def menu(menuid, **kwargs):
-	if menuid == "mainmenu":
-		return [(version.title, main, "mainmenu", 32)]
-	else:
-		return []
-	
-def eventinfo(session, servicelist, **kwargs):
-	ref = session.nav.getCurrentlyPlayingServiceReference()
-	print str(ref)
-	session.open(ArchivCZSKSearchClientScreen, ref)
-	
 def main(session, **kwargs):
 	ArchivCZSK(session)
 
-def startSetup(menuid, **kwargs):
+def menu(menuid, **kwargs):
 	if menuid == "mainmenu":
-		return [(version.description, main, "archivy_czsk", 32)]
-	return []
+		return [(DESCRIPTION, main, menuid, 32)]
+	else:
+		return []
+
+def eventInfo(session, servicelist, **kwargs):
+	ref = session.nav.getCurrentlyPlayingServiceReference()
+	session.open(ArchivCZSKSearchClientScreen, ref)
 
 def Plugins(path, **kwargs):
-	descr = version.description
-	nameA = version.title
-	list = [PluginDescriptor(where=[PluginDescriptor.WHERE_SESSIONSTART], fnc=sessionstart), PluginDescriptor(name=nameA, description=descr, where=PluginDescriptor.WHERE_PLUGINMENU, fnc=main, icon="czsk.png"), ]
-	if config.plugins.archivCZSK.extensions_menu.getValue():
-		list.append(PluginDescriptor(name=nameA, description=descr, where=PluginDescriptor.WHERE_EXTENSIONSMENU, fnc=main))
-	if config.plugins.archivCZSK.main_menu.getValue():
-		list.append(PluginDescriptor(name=nameA, description=descr, where=PluginDescriptor.WHERE_MENU, fnc=startSetup))
-	if config.plugins.archivCZSK.epg_menu.getValue():
-		list.append(PluginDescriptor(name=_("Search in ArchivCZSK"), description=descr, where=PluginDescriptor.WHERE_EVENTINFO, fnc=eventinfo))
+	list = [PluginDescriptor(where=[PluginDescriptor.WHERE_SESSIONSTART], fnc=sessionStart),
+		PluginDescriptor(NAME, description=DESCRIPTION, where=PluginDescriptor.WHERE_PLUGINMENU, fnc=main, icon="czsk.png")]
+	if config.plugins.archivCZSK.extensions_menu.value:
+		list.append(PluginDescriptor(NAME, where=PluginDescriptor.WHERE_EXTENSIONSMENU, fnc=main))
+	if config.plugins.archivCZSK.main_menu.value:
+		list.append(PluginDescriptor(NAME, where=PluginDescriptor.WHERE_MENU, fnc=menu))
+	if config.plugins.archivCZSK.epg_menu.value:
+		list.append(PluginDescriptor(_("Search in ArchivCZSK"), where=PluginDescriptor.WHERE_EVENTINFO, fnc=eventInfo))
 	return list
 
 
-if config.plugins.archivCZSK.preload.getValue() and not ArchivCZSK.isLoaded():
+if config.plugins.archivCZSK.preload.value and not ArchivCZSK.isLoaded():
 	ArchivCZSK.load_repositories()
 	ArchivCZSK.load_skin()
