7d40d8e939727a7828bffee44331b061c7748479
misanov <annonce@prolidi.cz>
2020-11-16 18:55:48 +0100

    release 1.3.6

--- build/plugin/src/version.py
+++ build/plugin/src/version.py
@@ -1,7 +1,7 @@
 from . import _
 
 title = _("ArchivCZSK")
-version = "1.3.5"
+version = "1.3.6"
 author = "mx3L,misanov"
 description = _("Playing CZ/SK archives")
 url = "https://github.com/mx3L/archivczsk/"
